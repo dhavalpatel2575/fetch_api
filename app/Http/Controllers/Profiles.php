a79bb9ad3ded32e46ce7709da9f46cf431bd3596
dhavalpatel2575 <71452353+dhavalpatel2575@users.noreply.github.com>
2020-11-17 13:55:22 -0500

    fetch api

---
+++ app/Http/Controllers/Profiles.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class Profiles extends Controller
+{
+    //
+    function list()
+    {
+        $data= Http::get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=7527a116751a014472f4cf9c7073cef3');
+   //     return "Hello";
+        return view('profile', ['data'=>$data]);
+    }
+}
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,3 @@
+ <h1>hello</h1>
+
+ {{print_r($data)}}
